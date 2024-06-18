4de2cd1ce5ea213007754396d492f235a851d608
Alain Paul <131689456+thealainpaul@users.noreply.github.com>
2024-06-18 11:01:48 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var link = top.document.createElement("link");
-  link.type = "image/favicon.ico";
+  link.type = "image/x-icon";
   link.rel = "shortcut icon";
   link.href = "./favicon.ico";
   top.document.getElementsByTagName("head")[0].appendChild(link);
