8281ee52179836caf9d32c1afd44a1dcb9e282f4
Alain Paul <131689456+thealainpaul@users.noreply.github.com>
2024-06-18 10:57:35 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ window.onload = function() {
   var link = top.document.createElement("link");
   link.type = "image/x-icon";
   link.rel = "shortcut icon";
-  link.href = "./Australian_Kangaroo.ico";
+  link.href = "./favicon.ico";
   top.document.getElementsByTagName("head")[0].appendChild(link);
 }
