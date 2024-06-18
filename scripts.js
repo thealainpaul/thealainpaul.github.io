a923f8f00525fbc9103b2028fd660a5f00a7bf42
Alain Paul <131689456+thealainpaul@users.noreply.github.com>
2024-06-18 10:59:38 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var link = top.document.createElement("link");
-  link.type = "image/x-icon";
+  link.type = "image/favicon.ico";
   link.rel = "shortcut icon";
   link.href = "./favicon.ico";
   top.document.getElementsByTagName("head")[0].appendChild(link);
